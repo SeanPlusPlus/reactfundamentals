123dadb3d82e87a1514f3eb438c44455c5822a52
Sean Stephenson <sms@pixar.com>
2016-06-03 12:42:37 -0700

    add default prop types

--- App.js
+++ App.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+
 class App extends React.Component {
   render(){
-    return <div>Hello</div>
+    let txt = this.props.txt;
+    return (
+      <div>
+        <h1>Hell World</h1>
+        <h3>{txt}</h3>
+      </div>
+    );
   }
 }
 
-export default App
+App.propTypes = {
+  txt: React.PropTypes.string,
+  cat: React.PropTypes.number.isRequired,
+}
+
+App.defaultProps = {
+  txt: 'this is the default'
+}
+
+ReactDOM.render(
+  <App cat={42} />,
+  document.getElementById('app')
+)
--- main.js
+++ main.js
@@ -1,4 +1,1 @@
-import React from 'react';
-import ReacDOM from 'react-dom';
 import App from './App';
-ReacDOM.render(<App />, document.getElementById('app'))
