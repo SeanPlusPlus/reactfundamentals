7e1b22d11c73dec00b8678c79c58929e8337b637
Sean Stephenson <sms@pixar.com>
2016-06-03 12:49:04 -0700

    add state update example

--- App.js
+++ App.js
@@ -2,12 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      txt: 'this is the state txt',
+      cat: 0
+    }
+  }
+  update(e){
+    this.setState({txt: e.target.value})
+  }
   render(){
     let txt = this.props.txt;
+    let cat = this.props.cat;
     return (
       <div>
-        <h1>Hell World</h1>
+        <input type="text"
+          onChange={this.update.bind(this)} />
+        <h1>State:</h1>
+        <h3>{this.state.txt}</h3>
+        <h3>{this.state.cat}</h3>
+        <hr />
+        <h1>Props:</h1>
         <h3>{txt}</h3>
+        <h3>{cat}</h3>
       </div>
     );
   }
@@ -19,7 +37,7 @@ App.propTypes = {
 }
 
 App.defaultProps = {
-  txt: 'this is the default'
+  txt: 'this is the props txt'
 }
 
 ReactDOM.render(
