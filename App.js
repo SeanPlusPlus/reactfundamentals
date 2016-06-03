c57401488fabaf10842d4087505d2fe99f8ae202
Sean Stephenson <sms@pixar.com>
2016-06-03 14:53:28 -0700

    add Widget child componenet

--- App.js
+++ App.js
@@ -5,42 +5,33 @@ class App extends React.Component {
   constructor(){
     super();
     this.state = {
-      txt: 'this is the state txt',
-      cat: 0
+      txt: '',
     }
+    this.update = this.update.bind(this)
   }
   update(e){
     this.setState({txt: e.target.value})
   }
   render(){
-    let txt = this.props.txt;
-    let cat = this.props.cat;
     return (
       <div>
-        <input type="text"
-          onChange={this.update.bind(this)} />
-        <h1>State:</h1>
-        <h3>{this.state.txt}</h3>
-        <h3>{this.state.cat}</h3>
-        <hr />
-        <h1>Props:</h1>
-        <h3>{txt}</h3>
-        <h3>{cat}</h3>
+        <Widget txt={this.state.txt} update={this.update} />
       </div>
     );
   }
 }
 
-App.propTypes = {
-  txt: React.PropTypes.string,
-  cat: React.PropTypes.number.isRequired,
-}
-
-App.defaultProps = {
-  txt: 'this is the props txt'
+const Widget = (props) => {
+  return (
+    <div>
+      <input type="text"
+        onChange={props.update} />
+      <h3>{props.txt}</h3>
+    </div>
+  )
 }
 
 ReactDOM.render(
-  <App cat={42} />,
+  <App />,
   document.getElementById('app')
 )
